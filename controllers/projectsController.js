8ca8dce9bc8ebedbf2130583884f51ce6152d139
Nicole Chiang <nic.chiang@gmail.com>
2016-03-07 12:35:58 -0500

    tasks get and post working

--- controllers/projectsController.js
+++ controllers/projectsController.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var passport = require("passport");
 var User = require("../models/users.js");
 var Project = require('../models/projects.js');
+var Task = require('../models/tasks.js');
 
 
 // router.get("/", function(req, res) {
@@ -68,6 +69,55 @@ router.delete("/project/:project_id", function(req, res) {
   })
 
 });
+
+
+// TASKS ROUTES
+
+
+// create
+router.post("/tasks/:pid", function(req, res) {
+  console.log(req.body);
+
+
+  // create new task
+  Task.create(req.body, function(err, task) {
+
+    // push task to tasks in project model
+    Project.findById(req.params.pid, function(err, project) {
+      project.tasks.push(task);
+      project.save(function(err, data) {
+        console.log("task saved");
+
+
+        // update the project in user's task array
+        // User.findById(req.user._id, function(err, user) {
+        //   user.update({})
+
+        //   console.log("user, ", user);
+        // })
+
+        User.update({_id: req.user._id, "projects._id": req.params.pid}, {$push: {"projects.$.tasks": task}}, function(err, data) {
+          console.log("user update data: ", data);
+
+          res.send("created task")
+
+        })
+      })
+    })
+  })
+})
+
+// get
+router.get("/tasks/:pid", function(req, res) {
+  console.log("get params, ", req.params.pid)
+  Project.findById(req.params.pid, function(err, project) {
+    console.log(project);
+    res.send(project);
+  })
+
+})
+
+
 
 
 
--- public/js/tasks_directive.js
+++ public/js/tasks_directive.js
@@ -16,13 +16,58 @@ app.controller("TaskController", ["$http", "$scope", function($http, $scope) {
   var controller = this;
   this.test = "task controller";
 
+  // get project object
   $scope.$on("project-data", function(eventObject, project) {
     console.log("task controller, ", project);
-
     controller.project = project;
+    controller.projId = project._id;
+  });
 
 
-  })
+  // fetch and load all the tasks
+  this.getTasks = function() {
+    $http({
+    method: "GET",
+    url: "/projects/tasks/" + controller.projId  // this path will change
+    }).then(
+      // success function
+      function(response) {
+        console.log("get response, ", response.data.tasks);
+        controller.tasks = response.data.tasks;
+
+      // error function
+      }, function(error) {
+        console.log(error);
+    });
+  }
+
+  // this.getTasks();
+
+  // create new task
+  this.newTask = {
+    name: null
+  }
+
+
+  this.addTask = function() {
+    console.log("PROJECT ID", controller.projId)
+    $http({
+    method: "POST",
+    url: "/projects/tasks/" + controller.projId, 
+    data: this.newTask
+    }).then(
+      // success function
+      function(response) {
+        console.log(response);
+        controller.getTasks();
+
+      // error function
+      }, function(error) {
+        console.log(error)
+
+    });
+  };
+
 
 
 
