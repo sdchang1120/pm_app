f9a11f4b6ba3bde4d955fbf9ec57f00140cb8d62
Sharon Chang <changsharond@gmail.com>
2016-03-10 22:54:41 -0500

    cleaning up server.js

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
-// REQUIREMENTS
+// ==============================
+//          REQUIREMENTS
+// ==============================
+
 var express = require('express');
 var bodyParser = require("body-parser");
 var cookieParser = require("cookie-parser");
@@ -6,12 +9,15 @@ var mongoose = require("mongoose");
 var passport = require("passport");
 var session = require("express-session");
 
-
 var mongoUri = process.env.MONGOLAB_URI || "mongodb://localhost:27017/pm_app"
 var port = process.env.PORT || 3000;
 var app = express();
 
-// MIDDLEWARE
+
+// ==============================
+//           MIDDLEWARE
+// ==============================
+
 // connect to mongo
 mongoose.connect(mongoUri);
 
@@ -33,17 +39,24 @@ app.use(session({ secret: "secret", resave: true, saveUninitialized: true })); /
 app.use(passport.initialize());
 app.use(passport.session());
 
-// CONTROLLERS
 
+// ==============================
+//          CONTROLLERS
+// ==============================
+
+// USER'S CONTROLLER
 var usersController = require("./controllers/usersController.js");
 app.use("/users", usersController);
 
+// PROJECT'S CONTROLLER
 var projectsController = require("./controllers/projectsController.js");
 app.use("/projects", projectsController);
 
 
+// ==============================
+//          CONNECTION
+// ==============================
 
-// LISTEN
 mongoose.connection.once('open', function() {
   app.listen(port, function() {
     console.log('LISTENING ON PORT: ', port);
